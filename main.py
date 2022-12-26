7d01854f40d6f64ee3c1e275c46b63001f424b4a
Medvate <ilia.bezverzhenko@mail.ru>
2022-12-26 22:15:23 +0300

    ✨ Add launch of service to main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import asyncio
+
+import uvicorn
 from fastapi import FastAPI
+from pyaml_env import parse_config
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+from services.jwt_auth import JWT_ROUTER, create_config_clients
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+CONFIG = parse_config("config.yaml")
+FRS_APP = FastAPI(title="Facial Recognition System (FastAPI + OpenCV)")
+FRS_APP.include_router(JWT_ROUTER)
+
+
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(create_config_clients(
+        clear_db=CONFIG['fastapi_service']['clear_db']
+    ))
+    loop.close()
+
+    uvicorn.run(
+        'main:FRS_APP',
+        host=CONFIG['fastapi_service']['host'],
+        port=CONFIG['fastapi_service']['port'],
+        reload=CONFIG['fastapi_service']['reload'],
+        workers=CONFIG['fastapi_service']['workers_count']
+    )
