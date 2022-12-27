c6641a7bd16ac142ea8db2be1e64ff30b01b8567
Medvate <ilia.bezverzhenko@mail.ru>
2022-12-27 17:31:20 +0300

    🔧 Update __all__ of services.jwt_auth

--- services/jwt_auth/__init__.py
+++ services/jwt_auth/__init__.py
@@ -1,7 +1,9 @@
 from ._routes import JWT_ROUTER, create_config_clients
+from ._jwt_logic import get_current_client
 
 
 __all__ = (
     "JWT_ROUTER",
-    "create_config_clients"
+    "create_config_clients",
+    "get_current_client"
 )
