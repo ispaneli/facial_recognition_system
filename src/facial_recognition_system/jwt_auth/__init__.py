4eb46d032d9e7ce18600957a43b95cfbb7a6c9cf
Medvate <ilia.bezverzhenko@mail.ru>
2023-03-05 21:44:05 +0300

    ⚙️ Add short imports for JWT-auth

--- src/facial_recognition_system/jwt_auth/__init__.py
+++ src/facial_recognition_system/jwt_auth/__init__.py
@@ -1,6 +1,16 @@
-from .dependencies import get_current_client
+from .dependencies import (
+    encode_password,
+    verify_password,
+    decode_token,
+    generate_new_tokens,
+    get_current_client
+)
 
 
 __all__ = [
+    "encode_password",
+    "verify_password",
+    "decode_token",
+    "generate_new_tokens",
     "get_current_client"
 ]
