cd3a40c716baaf606004a9eb77f1a9bcb59a24f9
Medvate <ilia.bezverzhenko@mail.ru>
2023-03-06 01:05:09 +0300

    ✨ Add date validation

--- src/facial_recognition_system/employee/schemas.py
+++ src/facial_recognition_system/employee/schemas.py
@@ -1,6 +1,10 @@
 from datetime import date
 
-from pydantic import BaseModel, Field
+from pydantic import (
+    BaseModel,
+    Field,
+    root_validator
+)
 
 
 class EmployeeModel(BaseModel):
@@ -10,13 +14,25 @@ class EmployeeModel(BaseModel):
     first_name: str
     second_name: str
 
-    date_of_birth: date = None
+    date_of_birth: str | date = None
     phone: int = None
     email: str = None
     home_address: str = None
 
     position: str = None
     other_info: str = None
+
+    @root_validator(pre=True)
+    def root_validator(cls, values: dict[str, ...]) -> dict[str, ...]:
+        """
+        Root validation of object (validation begins with it).
+
+        :param dict[str, ...] values: Values of all fields.
+        :return: Values of all fields after validation.
+        :rtype: dict[str, ...]
+        """
+        values['date_of_birth'] = str(values['date_of_birth'])
+        return values
 
 
 class UpdateEmployeeModel(EmployeeModel):
