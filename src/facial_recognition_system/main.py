d785e3c2f304829131ae42f289acd3582c5f9259
Medvate <ilia.bezverzhenko@mail.ru>
2023-03-05 23:40:26 +0300

    ⚙️ Update application setup

--- src/facial_recognition_system/main.py
+++ src/facial_recognition_system/main.py
@@ -1,28 +1,19 @@
-import asyncio
-
 import uvicorn
 from fastapi import FastAPI
 
-from services.jwt_auth import JWT_ROUTER, create_config_clients
-from services.employee import EMPLOYEE_ROUTER
-from services.face_auth import FR_ROUTER
-
 from src.facial_recognition_system.config import CONFIG
+from src.facial_recognition_system.employee import EMPLOYEE_ROUTER
+from src.facial_recognition_system.face_recognition import FACE_ROUTER
+from src.facial_recognition_system.jwt_auth import create_clients, JWT_ROUTER
 
 
 FRS_APP = FastAPI(title="Facial Recognition System (FastAPI + OpenCV)")
-FRS_APP.include_router(JWT_ROUTER)
-FRS_APP.include_router(EMPLOYEE_ROUTER)
-FRS_APP.include_router(FR_ROUTER)
+for router in (JWT_ROUTER, EMPLOYEE_ROUTER, FACE_ROUTER):
+    FRS_APP.include_router(router)
 
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(create_config_clients(
-        clear_db=CONFIG['fastapi_service']['clear_db']
-    ))
-    loop.close()
+    create_clients()
 
     uvicorn.run(
         'main:FRS_APP',
